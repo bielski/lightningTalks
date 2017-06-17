a3d1c7f3f31432e9e2ba2ae2724d771440c27654
Ewa Bielska <ewa@Ewas-MacBook-Pro.local>
2017-06-17 21:51:34 +0200

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Ewa Bielska on 6/17/17.
+//  Copyright © 2017 Ewa Bielska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
